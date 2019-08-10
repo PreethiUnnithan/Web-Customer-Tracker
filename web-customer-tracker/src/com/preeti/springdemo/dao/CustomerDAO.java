73265c3089fb8e6863e25e0e0cd8ee70f5ada5f1
preet <preet@LAPTOP-6ADH2U0D>
2019-08-10 17:50:37 +0800

    My first commit

---
+++ web-customer-tracker/src/com/preeti/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.preeti.springdemo.dao;
+
+import java.util.List;
+
+import com.preeti.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer>getCustomers();
+
+}
